aa73b12bbaeca528ebf6fd76e80acf1f6e15d9bc
laminemamadou01 <laminemamadou01@gmail.com>
2022-06-03 12:37:21 +0100

    first commit

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+     if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submit'])) {
+        
+         $conn= mysqli_connect('localhost','root','','test1') or die("connection Failed:" .mysqli_connect_error());
+        
+         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
+            $name= $_POST['name'];
+             $email= $_POST['email'];
+             $phone= $_POST['phone'];
+             $bgroupe= $_POST['bgroup'];
+
+             $sql="INSERT INTO users (name, email, phone, bgroupe) VALUES ('$name', '$email', '$phone', '$bgroupe')";
+
+             $query = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+             if($query) {
+                 echo 'Entry Successfull';
+             }
+             else {
+                 echo 'Error Occured';
+             }
+         }
+     }
+?>
